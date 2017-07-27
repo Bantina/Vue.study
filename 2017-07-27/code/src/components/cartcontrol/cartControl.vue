d2918363015a4e68fbb71e0559c724526a489940
bantina <827729574@qq.com>
2017-07-27 22:48:42 +0800

    goods,shopcart,cartcontrol comps of elm

---
+++ 2017-07-27/code/src/components/cartcontrol/cartControl.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="cartControl">
+    <!--<div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreaseCart($event)"></div>
+    <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
+    <div class="cart-add icon-add_circle" @click="addCart($event)"></div>-->
+    <!--添加动画后的加减数量控制组件-->
+    <transition name="move">
+      <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart($event)">
+        <span class="inner icon-remove_circle_outline"></span>
+      </div>
+    </transition>
+    <transition name="showUp">
+      <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
+    </transition>
+    <div class="cart-add icon-add_circle" @click="addCart($event)"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Vue from 'vue'
+
+  export default{
+      props:{
+          food:{ //count
+              type:Object
+          }
+      },
+      mounted(){
+          //console.log(this.food)
+      },
+      methods:{
+        addCart(event){
+          if(!event._constructed){ //自己的点击事件派发时 event._constructed==true
+            return; //pc监听到时return
+          }
+          if(!this.food.count){
+              //this.food.count = 1; //属性不存在时，直接控制是不起作用的；
+              Vue.set(this.food,'count',1); //需要Vue.set();
+          }else{
+              this.food.count++;
+          }
+          console.log('sss');
+        },
+        decreaseCart(event){
+          if(!event._constructed){ //自己的点击事件派发时 event._constructed==true
+            return; //pc监听到时return
+          }
+          if(this.food.count){
+            this.food.count--;
+          }
+        }
+      }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .cartControl
+    font-size :0
+    .cart-decrease
+      display: inline-block
+      padding: 6px
+      transition:all 0.4s linear
+      &.move-transition
+        opacity:1
+        -webkit-transform: translate3D(0, 0, 0)
+        -moz-transform: translate3D(0, 0, 0)
+        -ms-transform: translate3D(0, 0, 0)
+        -o-transform: translate3D(0, 0, 0)
+        transform: translate3D(0, 0, 0)
+      .inner
+        display: inline-block
+        line-height:24px
+        font-size :24px
+        color:rgb(0,160,220)
+        transition:all 0.4s linear
+        transform:rotate(0)
+      &.move-enter,&.move-leave
+        opacity:0
+        transform: translate3D(24px, 0, 0)
+        .inner
+          transform:rotate(180deg)
+    .cart-count
+      display: inline-block
+      vertical-align: top
+      width: 12px
+      padding-top:6px
+      line-height: 24px
+      text-align :center
+      font-size :10px
+      color:rgb(147,153,159)
+      transition:all 0.4s linear
+      &.showUp-transition
+        opacity:1
+      &.showUp-enter,&.showUp-leave
+        opacity:0
+    .cart-add
+      display: inline-block
+      padding: 6px
+      line-height:24px
+      font-size :24px
+      color:rgb(0,160,220)
+</style>
